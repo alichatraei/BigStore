13fa207594ce3c5a92adda3043193ed01a790675
Ali chatraei <avida.chatraee2001@yahoo.com>
2022-02-06 10:02:24 +0330

    Make loading component

--- client/src/features/Products/Catalog.tsx
+++ client/src/features/Products/Catalog.tsx
@@ -3,11 +3,17 @@ import { Grid } from '@mui/material';
 import IProduct from '../../app/models/product';
 import ProductCard from './ProductCard';
 import agent from '../../app/api/agent';
+import { toast } from 'react-toastify';
+import LoadingComponents from '../../app/layouts/LoadingComponents';
 const Catalog = () => {
     const [products, setProducts] = useState<IProduct[]>([]);
+    const [loading, setLoading] = useState<boolean>(true)
     useEffect(() => {
         agent.Product.allProducts().then(products => setProducts(products))
+            .catch(error => toast.error(error))
+            .finally(() => setLoading(false))
     }, []);
+    if (loading) return <LoadingComponents message='درحال بارگذاری محصولات...' />
     return (
         <Grid container spacing={2} my={3}>
             {products && products.map((item, index) =>
