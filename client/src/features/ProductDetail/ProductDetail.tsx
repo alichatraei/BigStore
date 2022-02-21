59538696050c5c9e6903c46ae68aa77d03d2d17f
Ali chatraei <avida.chatraee2001@yahoo.com>
2022-02-22 00:00:53 +0330

    Create basket summary component

--- client/src/features/ProductDetail/ProductDetail.tsx
+++ client/src/features/ProductDetail/ProductDetail.tsx
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from 'react'
-import { Grid, Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material'
+import { Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField } from '@mui/material'
 import IProduct from '../../app/models/product'
 import { useParams } from 'react-router-dom'
 import agent from '../../app/api/agent'
 import NotFoundComponent from '../../error/NotFoundComponent'
 import LoadingComponents from '../../app/layouts/LoadingComponents'
+import { useStoreContext } from '../../context/StoreContext'
+import { LoadingButton } from '@mui/lab'
 
 const ProductDetail = () => {
+    const { basket } = useStoreContext()
     const { id } = useParams<{ id: string }>()
     const [product, setProduct] = useState<IProduct | null>(null)
     const [loading, setLoading] = useState<boolean>(true)
+    const [quantity, setQuantity] = useState(0)
+    const [updateIndicator, setUpdateIndicator] = useState(false)
+    const item = basket?.items.find(item => item.productId === product?.id);
     useEffect(() => {
+        item && setQuantity(item.quantity)
         id && agent.Product.customProduct(parseInt(id))
             .then(product => setProduct(product))
             .catch()
             .finally(() => setLoading(false))
-    }, [id])
+    }, [id, item])
     if (loading) return (<LoadingComponents message='در حال بارگذاری محصول...' />)
     if (!product) return (<NotFoundComponent />)
     return (
@@ -50,6 +57,20 @@ const ProductDetail = () => {
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <Grid container spacing={2} mt={3}>
+                    <Grid item xs={6}>
+                        <TextField type="number" variant='outlined' label="تعداد موجود در سبد خرید" fullWidth
+                            value={quantity} size="small" />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <LoadingButton color="primary"
+                            variant="contained"
+                            fullWidth
+                            sx={{ padding: '8px' }}>
+                            {item ? "بروز کردن سبد خرید" : "اضافه کردن به سبد خرید"}
+                        </LoadingButton>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     )
