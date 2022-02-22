10754253b57cdef78be08037ad3bcfc946feaf79
Ali chatraei <avida.chatraee2001@yahoo.com>
2022-02-22 09:53:02 +0330

    Complete product detail ( add to basket text field & button )

--- client/src/features/ProductDetail/ProductDetail.tsx
+++ client/src/features/ProductDetail/ProductDetail.tsx
@@ -9,13 +9,33 @@ import { useStoreContext } from '../../context/StoreContext'
 import { LoadingButton } from '@mui/lab'
 
 const ProductDetail = () => {
-    const { basket } = useStoreContext()
+    const { basket, setBasket, removeItem } = useStoreContext()
     const { id } = useParams<{ id: string }>()
     const [product, setProduct] = useState<IProduct | null>(null)
     const [loading, setLoading] = useState<boolean>(true)
     const [quantity, setQuantity] = useState(0)
     const [updateIndicator, setUpdateIndicator] = useState(false)
-    const item = basket?.items.find(item => item.productId === product?.id);
+    const item = basket?.items.find(item => item.productId === product?.id)
+    const handleOnChangeTextField = (event: any) => {
+        if (event.target.value >= 0)
+            setQuantity(event.target.value)
+    }
+    const handleUpdateButton = () => {
+        setUpdateIndicator(true)
+        if (!item || quantity > item.quantity) {
+            const updateQuantity = item ? quantity - item.quantity : quantity
+            agent.basket.addItemToBasket(product?.id!, updateQuantity).then(basket => setBasket(basket))
+                .catch(error => console.log(error))
+                .finally(() => { setUpdateIndicator(false) })
+        }
+        else {
+            const updateQuantity = item.quantity - quantity;
+            agent.basket.removeItemFromBasket(product?.id!, updateQuantity)
+                .then(() => removeItem(product?.id!, updateQuantity))
+                .catch(error => console.log(error))
+                .finally(() => setUpdateIndicator(false))
+        }
+    }
     useEffect(() => {
         item && setQuantity(item.quantity)
         id && agent.Product.customProduct(parseInt(id))
@@ -59,12 +79,16 @@ const ProductDetail = () => {
                 </TableContainer>
                 <Grid container spacing={2} mt={3}>
                     <Grid item xs={6}>
-                        <TextField type="number" variant='outlined' label="تعداد موجود در سبد خرید" fullWidth
+                        <TextField type="number"
+                            onChange={handleOnChangeTextField} variant='outlined' label="تعداد موجود در سبد خرید" fullWidth
                             value={quantity} size="small" />
                     </Grid>
                     <Grid item xs={6}>
                         <LoadingButton color="primary"
+                            onClick={handleUpdateButton}
+                            loading={updateIndicator}
                             variant="contained"
+                            disabled={item?.quantity === quantity || !item && quantity === 0}
                             fullWidth
                             sx={{ padding: '8px' }}>
                             {item ? "بروز کردن سبد خرید" : "اضافه کردن به سبد خرید"}
